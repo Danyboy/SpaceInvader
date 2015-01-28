e84042a3b682e70f4e8d0185444779b6bfcd9a20
Daniil Mikhailov <danil@etersoft.ru>
2015-01-28 23:47:51 +0300

    All const must be in this file

---
+++ src/com/efnez/SpaceInvader/MyConstant.java
@@ -0,0 +1,12 @@
+package com.efnez.SpaceInvader;
+
+/**
+ * Created by Dany on 28.01.2015
+ */
+public class MyConstant {
+
+    public static float defaultBulletLength = 10;
+    public static float defaultTriangleLength = defaultBulletLength * 10;
+    public static float defaultTriangleAimLength = defaultTriangleLength * 1.4F;
+    public static float defaultWarriorLength = defaultBulletLength * 5;
+}
